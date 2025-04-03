379c8ef4c0ee8848abff5ca401a566cc50728afb
darkhacker1906 <singhabhinandan634@gmail.com>
2025-04-03 17:29:53 +0530

    Added the pinia for state management

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="bg-red-300">
     This is my app component.
+    <Test/>
   </div>
 </template>
 
 <script setup>
+import Test from './components/Test.vue'
 
 </script>
 
---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>Count: {{ counterStore.count }}</p>
+        <button @click="counterStore.increment">Increment</button>
+    </div>
+</template>
+
+<script setup>
+import { useCounterStore } from '../store/counterStore';
+
+const counterStore = useCounterStore();
+</script>
+
+<style scoped>
+
+</style>
